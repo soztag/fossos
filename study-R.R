163bb4dd9506f0212372bf062e1366223887ede6
Elen93 <40487564+Elen93@users.noreply.github.com>
2019-01-20 16:52:01 +0100

    Study_Eleni

---
+++ study-R.R
@@ -0,0 +1,12 @@
+study <- data.frame(
+  name = c("Moritz","Eleni","Momoko","Fabi","Makiko","Sam","Max"),
+  shoesize = c(42, 40, 37, 43, 36, 37, 42.5),
+  height = c(170, 169, 156, 179, 161, 168, 178),
+  age = c(26, 25, 21, 25, 32, 21, 35),
+  studys_major = c("Geography", "Sociology", "Germanistic", "Sociology", "Sociology", "Bigi Hums", "NA"),
+  earlobe_attached = c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE),
+  diopter = c(-1.75, -3.75, NA, NA, NA, -6.75, NA)
+)
+study
+
+  stringsAsFactors = FALSE

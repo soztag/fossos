6efc388afecf44c0008c69310d64e3493c6a3a8b
Ch1pzZz <44197143+Ch1pzZz@users.noreply.github.com>
2019-01-20 16:54:28 +0100

    Created DataFrame

---
+++ TestFile1.R
@@ -0,0 +1,12 @@
+df <- data.frame(
+  Name = c("Moritz", "Eleni", "Momoco", "Fabi", "Makiko", "Sam", "Max"),
+  Shoesize = c(42, 40, 37, 43, 36, 37, 42.5),
+  Height = c(170, 169, 156, 179, 161, 168, 178),
+  Age = c(26, 25, 21, 25, 32, 21, 35),
+  StudiesMajor = c("Geography", "Sociology", "Germanistik", "Sociology", "Sociology","DigiHums", NA),
+  Earlobe_Attached = c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE),
+  Dioptrien = c(-1.75, -3.75, NA, NA, NA, -6.75, NA),
+  stringsAsFactors = FALSE
+)
+
+df
